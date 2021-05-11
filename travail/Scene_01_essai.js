e5e7e4eb6beb7bc418e5bf27f799c5d586bc63f4
Médé <medericcardon@gmail.com>
2021-05-11 21:04:27 +0200

    mecaniques_alpha

--- travail/Scene_01_essai.js
+++ travail/Scene_01_essai.js
@@ -17,6 +17,14 @@ var bullet;
 var scoreBanane = 10;
 var texteBanane;
 
+var activeLevier2 = false;
+var activLevier3 = false;
+
+var walkWall = true;
+
+
+
+
 class Scene_01 extends Phaser.Scene{
     constructor(){
         super("Scene_01");
@@ -24,10 +32,10 @@ class Scene_01 extends Phaser.Scene{
     init(data){
     }
     preload(){
-        this.load.image('tiles', 'place_holder_2.png');
+        this.load.image('tiles', 'assets/place_holder_2.png');
         this.load.tilemapTiledJSON('scene_01_placeholder', 'scene_01.json');
-        this.load.image('player','player.png');
-        this.load.image('banane','banane_01.png');
+        this.load.image('player','assets/player.png');
+        this.load.image('banane','assets/banane_01.png');
     }
 
     create(){
@@ -38,12 +46,14 @@ class Scene_01 extends Phaser.Scene{
         const plateforme_fix = map.createLayer('plateforme_fix', tileset, 0, 0);
         const levier1 = map.createLayer('levier1', tileset, 0, 0);
         const levier2 = map.createLayer('levier2', tileset, 0, 0);
+        const levier3 = map.createLayer('levier3', tileset, 0, 0);
 
         plateforme_fix.setCollisionByExclusion(-1, true);
         plateforme_mouvement.setCollisionByExclusion(-1, true);
         plateforme_mouvement2.setCollisionByExclusion(-1, true);
         levier1.setCollisionByExclusion(-1, true);
         levier2.setCollisionByExclusion(-1, true);
+        levier3.setCollisionByExclusion(-1, true);
 
         player = this.physics.add.sprite(100,100,'player');
         player.body.setAllowGravity(true);
@@ -53,7 +63,7 @@ class Scene_01 extends Phaser.Scene{
         groupeBullets = this.physics.add.group();
 
         banane = this.physics.add.sprite(700,200,'banane');
-
+        this.physics.add.collider(plateforme_fix,player,wall,null,this);
         this.physics.add.collider(plateforme_fix,player);
         this.physics.add.collider(plateforme_fix,banane);
         this.physics.add.overlap(banane,player,dropBanane,null,this)
@@ -67,16 +77,18 @@ class Scene_01 extends Phaser.Scene{
         this.physics.add.collider(plateforme_mouvement,groupeBullets);
         this.physics.add.collider(player,groupeBullets);
 
+        this.physics.add.collider(levier3,groupeBullets,levierOn,null,this);
+
         cursors = this.input.keyboard.createCursorKeys();
         space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
         boutonTire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
 
-        texteBanane = this.add.text(40,28, scoreBanane,{font: '20px Georgia', fill: '#f0acdc' });
+        texteBanane = this.add.text(40,28, scoreBanane,{font: '20px Georgia', fill: '#f0acdc' }).setScrollFactor(0);
 
         tween_platmove1 = this.tweens.add({
             targets: plateforme_mouvement,
-            y: 50,
+            y: -150,
             duration: 4000,
             yoyo: true,
             paused: true,
@@ -85,18 +97,23 @@ class Scene_01 extends Phaser.Scene{
 
         tween_platmove2 = this.tweens.add({
             targets: plateforme_mouvement2,
-            y: 150,
+            y: -150,
             duration: 4000,
-            yoyo: true,
             paused: true,
-            repeat: -1
+            repeat: 0
         });
+
+        this.cameras.main.setBounds(0, 0,  2940  , 1960 );
+        this.physics.world.setBounds(0, 0, 2940 , 1960);
+        this.cameras.main.startFollow(player, true, 0.05, 0.05);
+        this.cameras.main.fadeIn(2000);
 
     }
 
     
 
     update(){
+
 
         if(bulletOn == false){ // relance du compteur des projectiles //
             compteurBullet-- ;
@@ -110,12 +127,17 @@ class Scene_01 extends Phaser.Scene{
         if ( Phaser.Input.Keyboard.JustDown(boutonTire)) {// Appuyer sur ESPACE permet de lancer un projectile //
             tirer(player);
         }
-
-        if (cursors.left.isDown)  {
+        if(cursors.left.isDown && player.body.blocked.left && walkWall == true){
+            player.setVelocityY(-100);
+        }
+        else if (cursors.left.isDown)  {
             player.setVelocityX(-250);
             player.direction = 'left';
             player.setBounce(0.1);
 
+        }
+        else if(cursors.right.isDown && player.body.blocked.right && walkWall == true){
+            player.setVelocityY(-200);
         }
         else if (cursors.right.isDown) {
 
@@ -130,7 +152,7 @@ class Scene_01 extends Phaser.Scene{
         else  {
             player.setVelocityX(0);
         }
-        if (space.isDown) {
+        if (Phaser.Input.Keyboard.JustDown(cursors.space)) {
             player.setVelocityY(-300);
             player.setBounce(0.1);
         }  
@@ -138,14 +160,20 @@ class Scene_01 extends Phaser.Scene{
 }
 
 function actionLevier1(){
+    walkWall = false;
     if(keyE.isDown){
         tween_platmove1.play(); 
     }
 }
 
+
 function actionLevier2(){
+    walkWall = false;
     if(keyE.isDown){
-        tween_platmove2.play();
+        activeLevier2 = true;
+        if(activeLevier2 == true && activLevier3 == true){
+            tween_platmove2.play();
+        }
     }
 }
 
@@ -177,7 +205,20 @@ function bulletDestroy(){
 
 function dropBanane(){
     banane.destroy(true,true);
-    scoreBanane += 7 
+    scoreBanane += 7;
     texteBanane.setText(scoreBanane);
+}
+
+function levierOn(levier3){
+    activLevier3 = true;
+    bulletDestroy();
+    levier3.setTint(0xff0000);
+    if(activeLevier2 == true && activLevier3 == true){
+        tween_platmove1.play(); 
+    }   
+}
+
+function wall(){
+    walkWall = true;
 }
 
--- travail/assets/Tiled/place_holder_02.tsx
+++ travail/assets/Tiled/place_holder_02.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="place_holder_02" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="../Assets/place_holder_2.png" width="96" height="32"/>
+<tileset version="1.5" tiledversion="1.5.0" name="place_holder_2" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../place_holder_2.png" width="96" height="32"/>
 </tileset>
